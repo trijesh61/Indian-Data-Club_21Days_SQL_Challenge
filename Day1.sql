8e7812b4fdb13be7d6d3e4ad83d750c29d4b18a4
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-03 16:38:06 +0530

    Update Day1.sql

--- Day1.sql
+++ Day1.sql
@@ -3,3 +3,10 @@ SELECT * FROM patients;
 
 -- Select only the patient_id, name, and age columns from the patients table.
 SELECT patient_id,name,age FROM patients;
+
+-- Display the first 10 records from the services_weekly table.
+SELECT * FROM services_weekly LIMIT 10;
+
+-- Day 1 Challange:
+-- List all unique hospital services available in the hospital.
+SELECT distinct service FROM services_weekly;
