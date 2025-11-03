2c71f00ae20fa944c45e7a4b113ca402fab05ab9
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-03 15:52:26 +0530

    Create Day1.sql

---
+++ Day1.sql
@@ -0,0 +1,5 @@
+-- Retrieve all columns from the patients table.
+SELECT * FROM patients;
+
+-- Select only the patient_id, name, and age columns from the patients table.
+SELECT patient_id,name,age FROM patients;
