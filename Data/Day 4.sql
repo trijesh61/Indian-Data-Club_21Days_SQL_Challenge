07218e6fb39af81c3e669eb198addba0a4910904
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-06 13:34:04 +0530

    Update Day 4.sql

--- Data/Day 4.sql
+++ Data/Day 4.sql
@@ -15,4 +15,13 @@ ORDER BY arrival_date DESC
 LIMIT 10;
 
 
+
+
 -- Day 4 Challange:
+-- Find the 3rd to 7th highest patient satisfaction scores from the patients table, showing patient_id, name, service, and satisfaction.
+-- Display only these 5 records.
+SELECT patient_id,name,service,satisfaction
+FROM patients
+ORDER BY satisfaction DESC
+LIMIT 5 OFFSET 2;
+
