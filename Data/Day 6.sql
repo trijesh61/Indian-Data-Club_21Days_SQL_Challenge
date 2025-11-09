99fc0602118ee1b300ae98341f4ae4ce9ae01add
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-09 10:20:49 +0530

    Update Day 6.sql

--- Data/Day 6.sql
+++ Data/Day 6.sql
@@ -2,3 +2,10 @@
 SELECT service, COUNT(patient_id) AS No_of_Patients 
 FROM patients
 GROUP BY service;
+
+-- Calculate the average age of patients grouped by service.
+SELECT service, AVG(age) AS AVG_Age_of_Patients 
+FROM patients
+GROUP BY service;
+
+
