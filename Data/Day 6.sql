5416214d9f0252df05121c56c4b4d7c364cf2615
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-09 10:33:59 +0530

    Update Day 6.sql

--- Data/Day 6.sql
+++ Data/Day 6.sql
@@ -9,3 +9,25 @@ FROM patients
 GROUP BY service;
 
 
+-- Find the total number of staff members per role.
+SELECT role, COUNT(staff_id) AS No_of_Staff_members
+FROM staff
+GROUP BY role;
+
+
+
+-- Daily Challenge :
+-- For each hospital service, calculate the total number of patients admitted, total patients refused, 
+-- and the admission rate (percentage of requests that were admitted). 
+-- Order by admission rate descending.
+SELECT
+    service,
+    SUM(patients_admitted) AS Patients_Admitted,
+    SUM(patients_refused) AS Patients_Refused,
+    ROUND(
+        (SUM(patients_admitted) * 100.0) /
+        (SUM(patients_admitted) + SUM(patients_refused)), 2
+    ) AS Admission_Rate_Percentage
+FROM services_weekly
+GROUP BY service
+ORDER BY Admission_Rate_Percentage DESC;
