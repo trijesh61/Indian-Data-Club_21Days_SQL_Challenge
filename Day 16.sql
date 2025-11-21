257491ddfc92cccd0d10071f1674d649464b032f
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-21 16:43:31 +0530

    Update Day 16.sql

--- Day 16.sql
+++ Day 16.sql
@@ -40,10 +40,11 @@ WHERE p.service IN (
 );
 
 -- Daily Challenge: 
-/* Find all patients who were admitted to services that had at least one week where patients were refused AND 
-	the average patient satisfaction for that service was below the overall hospital average satisfaction. 
-	Show patient_id, name, service, and their personal satisfaction score. */
-    SELECT p.patient_id, p.name, p.service, p.satisfaction
+-- Find all patients who were admitted to services that had at least one week where patients were refused AND 
+-- the average patient satisfaction for that service was below the overall hospital average satisfaction. 
+-- Show patient_id, name, service, and their personal satisfaction score. 
+
+SELECT p.patient_id, p.name, p.service, p.satisfaction
 FROM patients p
 WHERE p.service IN (SELECT service
     FROM (SELECT service,
