fcd3b6a2f348df8ff2c18f3e440f08c2b0679641
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-27 21:44:08 +0530

    Update Day 21.sql

--- Day 21.sql
+++ Day 21.sql
@@ -11,3 +11,26 @@ WITH service_stats AS (
 )
 SELECT * 
 FROM service_stats;
+
+
+-- 2: Use multiple CTEs to break down a complex query into logical steps.
+WITH service_stats AS (
+    SELECT 
+        service,
+        SUM(patients_request)   AS total_patients_request,
+        SUM(patients_admitted)  AS total_patients_admitted,
+        SUM(patients_refused)   AS total_patients_refused,
+        ROUND(AVG(patient_satisfaction), 2) AS average_patient_satisfaction
+    FROM services_weekly
+    GROUP BY service
+),
+cte_rates AS (
+    SELECT 
+        service,
+        ROUND(total_patients_admitted * 1.0 / total_patients_request, 2) AS admission_rate,
+        ROUND(total_patients_refused  * 1.0 / total_patients_request, 2) AS refusal_rate,
+        RANK() OVER (ORDER BY total_patients_admitted DESC) AS ranking
+    FROM service_stats
+)
+SELECT *
+FROM cte_rates;
